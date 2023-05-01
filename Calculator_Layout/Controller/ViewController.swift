09453bd0b0c95063285bf0a3ab5dcceff428ae4b
Ojidaemo <vit.martsinovich@gmail.com>
2023-05-01 14:50:29 +0300

    Rewriting to MVC

---
+++ Calculator_Layout/Controller/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Calculator_Layout
+//
+//  Created by Vitali Martsinovich on 2023-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot be converted to a double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            } else if calcMethod == "AC" {
+                    displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayValue *= 0.01
+            }
+        }
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+    
+}
+
---
+++ Calculator_Layout/Model/CalculatorLogic.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorLogic.swift
+//  Calculator_Layout
+//
+//  Created by Vitali Martsinovich on 2023-05-01.
+//
+
+import UIKit
+
+class CalculatorLogic  {
+    
+}
